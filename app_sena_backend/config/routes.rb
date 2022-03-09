3a87c8e9ba9048568d94c56b10ee6a77d7c927d1
Steicy De La Torre <shechytorres@gmail.com>
2022-03-09 13:40:27 -0500

    main creado
    main creado

--- app_sena_backend/config/routes.rb
+++ app_sena_backend/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+  }
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   root "main#home"
+  # get 'users/sessions#destroy'
+
+  # get "users/sing_out#destroy"
   # get 'main/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
